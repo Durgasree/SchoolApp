e646b0af8dcdb3207203821ac91950abde7a410a
Durgasree <shree1499@gmail.com>
2016-07-11 11:33:04 +0530

    durgasree - modifications

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+  	case params[:page_name]
+  	when "home"
+  		render "home"
+  	when "contact"
+  		render "contact"
+  	when "services"
+  		render "services"
+  	when "team"
+  		render "team"
+  	when "portfolio"
+  		render "protofolio"
+  	else
+  		render "home"
+  	end 			
   end
 end
